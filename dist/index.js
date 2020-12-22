1d8dfd63db72c1c8bb9120fa704d3dffcd6536f7
Sergey Bondar <Bondar.S.Evg@sberbank.ru>
2020-12-22 15:18:21 +0300

    chore(ver): 4.0.0

--- dist/index.js
+++ dist/index.js
@@ -2,45 +2,49 @@
 
 var assign = require('object-assign');
 
-var postcss = require('postcss');
-
 var defaultOptions = require('./fontstacks-config.js');
 
 function comparableFontName(name) {
   return name.toLowerCase();
 }
 
-function expandFamily(decl, fontStacks) {
+function getExpandedFamily(decl, fontStacks) {
   if (decl.value.match(/,/)) {
-    return;
+    return null;
   }
 
   var fontName = comparableFontName(decl.value.replace(/["']/g, ''));
 
   if (fontStacks[fontName]) {
-    decl.value = fontStacks[fontName];
+    return fontStacks[fontName];
   }
+
+  return null;
 }
 
-function expandFontShorthand(decl, fontStacks) {
+function getExpandedFontShorthand(decl, fontStacks) {
   if (decl.value.match(/,/)) {
-    return;
+    return null;
   }
 
+  var result = decl.value;
   Object.keys(fontStacks).forEach(function (fontName) {
     var onlyOneFont = '(^|px\\s+)("|\')?' + fontName + '("|\')?(\\s*!important)?$'; // eslint-disable-next-line security/detect-non-literal-regexp
 
     var regEx = new RegExp(onlyOneFont, 'i');
-    decl.value = decl.value.replace(regEx, '$1' + fontStacks[fontName]);
+    result = result.replace(regEx, '$1' + fontStacks[fontName]);
   });
+  return result;
 }
 
-function transform(decl, fontStacks) {
+function getNewValue(decl, fontStacks) {
   if (decl.prop === 'font-family') {
-    expandFamily(decl, fontStacks);
+    return getExpandedFamily(decl, fontStacks);
   } else if (decl.prop === 'font') {
-    expandFontShorthand(decl, fontStacks);
+    return getExpandedFontShorthand(decl, fontStacks);
   }
+
+  return null;
 }
 
 function normalizeFontName(name) {
@@ -61,18 +65,21 @@ function getFontStack(opts) {
   return fontStacks;
 }
 
-module.exports = postcss.plugin('postcss-fontstack-auto', function (opts) {
+module.exports = function () {
+  var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
   var fontStacks = getFontStack(opts);
-  return function (css) {
-    css.walkRules(function (rule) {
-      // its ignore at-rules so it's ok
-      rule.walkDecls(function (decl) {
-        if (decl.type === 'decl') {
-          if (['font-family', 'font'].includes(decl.prop)) {
-            transform(decl, fontStacks);
-          }
+  return {
+    postcssPlugin: 'postcss-fontstack-auto',
+    Declaration: function Declaration(decl) {
+      if (decl.parent.type !== 'atrule' && ['font-family', 'font'].includes(decl.prop)) {
+        var newValue = getNewValue(decl, fontStacks);
+
+        if (newValue) {
+          decl.value = newValue;
         }
-      });
-    });
+      }
+    }
   };
-});
+};
+
+module.exports.postcss = true;
